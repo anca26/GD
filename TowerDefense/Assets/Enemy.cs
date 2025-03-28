d25b3cd0c7e2cba04c85c85e2869aeff6722b8ce
anca26 <anca.iacob26@gmail.com>
2025-03-28 23:27:02 +0200

    bug_fixed

--- TowerDefense/Assets/Enemy.cs
+++ TowerDefense/Assets/Enemy.cs
@@ -31,6 +31,7 @@ public class Enemy : MonoBehaviour
         if (wavepointIndex >= Waypoints.points.Length - 1)
         {
             Destroy(gameObject);
+            return;
         }
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
