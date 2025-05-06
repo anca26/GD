7ae718f0a80e94c8d6ea85959c5eadde7d42bb5e
anca26 <anca.iacob26@gmail.com>
2025-05-06 18:35:57 +0300

    HP bar for enemies

--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -1,24 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float startSpeed = 10f;
+
     [HideInInspector]
     public float speed;
 
-    public float HP = 100;
+    public float startHP = 100;
+    private float HP;
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
     void Start()
     {
         speed = startSpeed;
+        HP = startHP;
     }
 
     public void TakeDamage(float amount)
     {
         HP -= amount;
+        healthBar.fillAmount = HP / startHP;
         if (HP <= 0)
         {
             Die();
